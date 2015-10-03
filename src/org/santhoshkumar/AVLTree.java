ea3cbb693aab548d8a1f0ddb49ea2be257a8a299
Santhosh Kumar <santhosh.prakash@gmail.com>
2015-10-03 12:29:04 +0530

    implement insert

--- src/org/santhoshkumar/AVLTree.java
+++ src/org/santhoshkumar/AVLTree.java
@@ -2,35 +2,52 @@ package org.santhoshkumar;
 
 public class AVLTree {
 
+    Node root;
+
     public static void main(String[] args) {
 	// write your code here
     }
 
+    /*
+    //                Rotate Left
     //          z                    z
-    //        /  \                  / \
-    //      T4   [y]       =      T4  [x]
+    //         / \                  / \
+    //       T4  [y]      ->      T4  [x]
     //           /  \                /  \
-    //          T3  [x]            [y]  T2
+    //          T3  [x]            [y]  T1
     //              / \           /  \
-    //             T1 T2         T3  T1
-    public void leftRotation(Node y){
+    //             T2 T1         T3  T2
+    //
+    */
+
+    public Node leftRotation(Node y){
         Node x = y.right;
-        Node T1 = x.left;
+        Node T2 = x.left;
 
         //Rotation
         x.left = y;
-        y.right = T1;
+        y.right = T2;
 
+        //Update height
+        x.height = 1+Math.max(getHeight(x.left),getHeight(x.right));
+        y.height = 1+Math.max(getHeight(y.left),getHeight(y.right));
+
+        return x;
     }
 
-    //          z
-    //        /  \
-    //      [y]    T4
-    //     /  \
-    //   [x]    T3
-    //   / \
-    //  T1 T2
-    public void rightRotation(Node y){
+    /*
+    //               Rotate Right
+    //          z                   z
+    //        /  \                 / \
+    //      [y]   T4     ->      [x]  T4
+    //     /  \                /  \
+    //   [x]    T3            T1   [y]
+    //   / \                      /  \
+    //  T1 T2                    T2  T3
+    //
+    */
+
+    public Node rightRotation(Node y){
         Node x = y.left;
         Node T2 = x.right;
 
@@ -39,10 +56,24 @@ public class AVLTree {
         y.left = T2;
 
         //Update height
+        x.height = 1+Math.max(getHeight(x.left),getHeight(x.right));
+        y.height = 1+Math.max(getHeight(y.left),getHeight(y.right));
+
+        return x;
     }
 
-    public void insert(int data){
+    public int getHeight(Node node){
+        if(node == null){
+            return 0;
+        }
+        return node.height;
+    }
 
+    public Node insert(Node node, int data){
+        if(node == null){
+            return new Node(data);
+        }
+        return null;
     }
 
 }
@@ -54,6 +85,7 @@ class Node{
     int height;
     Node(int data){
         this.data = data;
+        this.height = 1;
     }
     Node(int data, int height){
         this.data = data;
